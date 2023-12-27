f02cfb14034b22cca1ec1a14a0f048aadb61ad36
ekkymulia <ekky.mulialasardi@gmail.com>
2023-12-27 13:54:37 +0700

    initial push

---
+++ Starterkit/resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+@section('title')
+    Dashboard
+@endsection
+@section('page-title')
+    Dashboard
+@endsection
+@section('body')
+
+    <body>
+    @endsection
+    @section('content')
+        
+        
+    @endsection
+    @section('scripts')
+        <!-- App js -->
+        <script src="{{ URL::asset('build/js/app.js') }}"></script>
+    @endsection
